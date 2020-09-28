ca113497f83ce24f2930c79c0a3c05ae387292d8
deccer <decccer@outlook.com>
2020-09-28 19:56:24 +0400

    Add project skeleton

---
+++ src/Nord.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Nord.Client
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var compositionRoot = CreateCompositionRoot();
+
+            using var game = compositionRoot.GetService<MainGame>();
+            game.Run();
+        }
+
+        private static IServiceProvider CreateCompositionRoot()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<MainGame>();
+            return services.BuildServiceProvider();
+        }
+    }
+}
