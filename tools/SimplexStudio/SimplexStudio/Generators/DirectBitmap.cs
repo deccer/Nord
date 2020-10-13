75d6608dc8fc4a64f1acc14d913fa0b85dbcbf0a
deccer <decccer@outlook.com>
2020-10-13 10:42:11 +0400

    Cleanup code

--- tools/SimplexStudio/SimplexStudio/Generators/DirectBitmap.cs
+++ tools/SimplexStudio/SimplexStudio/Generators/DirectBitmap.cs
@@ -8,9 +8,13 @@ namespace SimplexStudio.Generators
     public class DirectBitmap : IDisposable
     {
         public Bitmap Bitmap { get; }
-        public Int32[] Bits { get; }
+
+        public int[] Bits { get; }
+
         public bool Disposed { get; private set; }
+
         public int Height { get; }
+
         public int Width { get; }
 
         protected GCHandle BitsHandle { get; }
@@ -19,31 +23,34 @@ namespace SimplexStudio.Generators
         {
             Width = width;
             Height = height;
-            Bits = new Int32[width * height];
+            Bits = new int[width * height];
             BitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned);
             Bitmap = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb, BitsHandle.AddrOfPinnedObject());
         }
 
-        public void SetPixel(int x, int y, Color colour)
+        public void SetPixel(int x, int y, Color color)
         {
-            int index = x + (y * Width);
-            int col = colour.ToArgb();
+            int index = y * Width + x;
+            int col = color.ToArgb();
 
             Bits[index] = col;
         }
 
         public Color GetPixel(int x, int y)
         {
-            int index = x + (y * Width);
-            int col = Bits[index];
-            Color result = Color.FromArgb(col);
+            int index = y * Width + x;
+            int color = Bits[index];
 
-            return result;
+            return Color.FromArgb(color);
         }
 
         public void Dispose()
         {
-            if (Disposed) return;
+            if (Disposed)
+            {
+                return;
+            }
+
             Disposed = true;
             Bitmap.Dispose();
             BitsHandle.Free();
